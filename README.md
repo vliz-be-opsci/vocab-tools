9b4b51600d1b8c976d5469a557596764415bbea6
Marc Portier <marc.portier@gmail.com>
2024-07-19 18:00:20 +0200

    just getting started

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # vocab-tools
 general space to toy around with, dump, analyse, ... RDF vocabs
+
+
+## usage
+### dependencies
+This assumes you have [poetry](https://python-poetry.org/docs/#installing-with-the-official-installer) and [make](https://gnuwin32.sourceforge.net/packages/make.htm) installed.
+
+### setup
+
+```sh
+$ make install
+``` 
+
+### run 
+
+```sh
+$ ./dump.py # create asfa  and cabt csv dumps
+```
---
+++ dump.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+from sema.query import GraphSource, DefaultSparqlBuilder, QueryResult
+import pandas as pd
+from datetime import date
+from pathlib import Path
+from typing import Dict
+from logging import getLogger
+
+
+log = getLogger(__name__)
+TEMPLATES_FOLDER: Path = Path(__file__).parent / "sparql"
+sparqlbuilder: DefaultSparqlBuilder = DefaultSparqlBuilder(TEMPLATES_FOLDER)
+query: str = sparqlbuilder.build_syntax("skos-concepts.sparql")
+SEP: Dict[str, str] = dict(
+    cabt="https://id.cabi.org/PoolParty/sparql/cabt",
+    asfa="https://agrovoc.fao.org/sparql",
+)
+
+
+def make_skos_concepts_dump(sparql_endpoint: str, file_name: str):
+    try:
+        # make GraphSource for endpoint
+        source: GraphSource = GraphSource.build(sparql_endpoint)
+        # exec to df
+        result: QueryResult = source.query(query)
+        df: pd.DataFrame = result.to_dataframe()
+        # ensure location exists
+        Path(file_name).parent.mkdir(parents=True, exist_ok=True)
+        # write to csv
+        df.to_csv(file_name, index=False, encoding="utf-8")
+    except Exception as e:
+        log.exception(e)  # log but continue
+
+
+def main():
+    today = date.today().isoformat()
+    for name, sparql_endoint in SEP.items():
+        file_name = f"datadumps/{today}-{name}.csv"
+        make_skos_concepts_dump(sparql_endoint, file_name)
+
+
+if __name__ == "__main__":
+    main()
